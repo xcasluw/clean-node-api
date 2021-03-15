f008b79c4c3c04ed4c124e46585546ba9ffcb460
xcasl <oligena@hotmail.com>
2021-03-15 12:16:11 -0300

    feat: ensure SignUpController.handle implements correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,7 @@
+import { HttpResponse, httpRequest } from '../protocols/http'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: httpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
